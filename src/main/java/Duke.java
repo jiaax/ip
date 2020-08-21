12128e5e7251e31422e50e553e8e33d4dd280cb2
jiaax <ngjiaxin11@gmail.com>
2020-08-21 17:28:48 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,78 +3,28 @@ import java.util.ArrayList;
 
 public class Duke {
 
-    public static void main(String[] args) throws DukeException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> store = new ArrayList<>();
+        ArrayList<String> store = new ArrayList<>();
 
         System.out.println("Hello! I'm meimei ^_^\nI could scream at you all day!");
         String command = sc.nextLine();
 
         while(!command.equals("bye")) {
-            String key = command.split(" ",2)[0];
-            if (key.equals("list")) {
+            if (command.equals("list")) {
                 System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < store.size(); i++) {
                     System.out.println((i+1) + "." + store.get(i).toString());
                 }
-                command = sc.nextLine();
-            }
-            else if (key.equals("done")) {
-                int k = Integer.parseInt(command.split(" ")[1]);
-                store.get(k-1).markAsDone();
-                System.out.println("Nice! This task is marked as done!");
-                System.out.println(store.get(k-1));
-                command = sc.nextLine();
-            }
-            else if (key.equals("delete")) {
-                int k = Integer.parseInt(command.split(" ")[1]);
-                Task remov = store.get(k-1);
-                store.remove(remov);
-                System.out.println("Meimei will forget about this task!");
-                System.out.println(remov);
-                System.out.println("Now you have " + store.size() + " tasks in the list.");
-                command = sc.nextLine();
             }
             //adding of task
             else {
-                try {
-                    String remain = command.split(" ", 2)[1];
-                    if (key.equals("todo")) {
-                        store.add(new Todo(remain));
-                        System.out.println("Got it. I've added this task:\n" + store.get(store.size() - 1));
-                    } else {
-                        String description = remain.split("/", 2)[0];
-                        Task task = new Task("");
-
-                        if (key.equals("deadline")) {
-                            String by = remain.split("/by", 2)[1];
-                            task = new Deadline(description, by);
-                            store.add(task);
-                        }
-                        if (key.equals("event")) {
-                            String at = remain.split("/at", 2)[1];
-                            task = new Event(description, at);
-                            store.add(task);
-                        }
-                        System.out.println("Got it. I've added this task:\n" + task.toString());
-                    }
-                    System.out.println("Now you have " + store.size() + " tasks in the list.");
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    if (key.equals("todo")) {
-                        System.out.println(new DukeException("todo"));
-                    }
-                    else if (key.equals("deadline")) {
-                        System.out.println(new DukeException("deadline"));
-                    }
-                    else if (key.equals("event")) {
-                        System.out.println(new DukeException("event"));
-                    } else {
-                        System.out.println("Oops! Meimei does not understand this command");
-                    }
-                }
-                command = sc.nextLine();
+                store.add(command);
+                System.out.println("added: " + command);
             }
+            command = sc.nextLine();
         }
+
         System.out.println("Bye. Meimei will miss you!");
     }
 }
